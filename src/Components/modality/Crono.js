f72a91800ce9545b08c023b7994cab321c48c595
Welen1911 <welenalmeida2210@gmail.com>
2023-08-08 14:18:10 -0300

    Não resolvi o bug ainda

--- src/Components/modality/Crono.js
+++ src/Components/modality/Crono.js
@@ -3,34 +3,15 @@ import { useState } from "react"
 import { Button, Link } from "./Button"
 import { Cronometro } from "./Cronometro"
 
-const urlParams = new URLSearchParams(window.location.search)
-const modality = urlParams.get("modality")
+
 
 export const Crono = () => {
-  let modaDecre
-  try {
-    modaDecre = parseInt(modality)
-  } catch (e) {
-    console.log("Erro ao converter para inteiro!")
-  }
-  const [decre, setDecre] = useState(modaDecre)
-  const [stop, setStop] = useState(false)
-
-  const handleClick = () => {
-    if (decre == 1) {
-      setStop(true)
-      console.log(stop)
-    }
-    setDecre(decre-1)
-  } 
-  
 
   return (
-        <>
-        <div className="container">
-          <div><Cronometro stop={stop}></Cronometro></div>
-          <Button className="btn btn-dark mb-3 mt-3" onClick={() => handleClick()} id="button">{decre}</Button>
-        </div>
-        </>
-   ) 
+    <>
+      <div className="container">
+        <div><Cronometro></Cronometro></div>
+      </div>
+    </>
+  )
 }
--- src/Components/modality/Cronometro.js
+++ src/Components/modality/Cronometro.js
@@ -1,14 +1,39 @@
 import { useState } from "react";
 
+import { Button } from "./Button";
 
-
+const urlParams = new URLSearchParams(window.location.search)
+const modality = urlParams.get("modality")
 
 let [ss, mm, hh] = [0, 0, 0]
 let [dSs, dMm, dHh] = ['', '', '']
+let cont = 0
+export const Cronometro = () => {
+    let modaDecre
 
-export const Cronometro = ({stop}) => {
+    try {
+        modaDecre = parseInt(modality)
+    } catch (e) {
+        console.log("Erro ao converter para inteiro!")
+    }
+
+    const [decre, setDecre] = useState(modaDecre)
+    const [stop, setStop] = useState(false)
+
+    const handleClick = () => {
+        if (decre == 1) {
+            setStop(true)
+        }
+        setDecre(decre - 1)
+    }
+
+    const handleStop = () => {
+        clearInterval(intervalo)
+        intervalo = null
+        cont++
+    }
+
     const [tempo, setTempo] = useState("00:00:000")
-
     const atualizarDisplay = () => {
         hh < 10 ? dHh = "0" + hh : dHh = hh
         mm < 10 ? dMm = "0" + mm : dMm = mm
@@ -28,15 +53,27 @@ export const Cronometro = ({stop}) => {
         }
         atualizarDisplay()
     }
-    if (!stop) {
+
+    let intervalo = () => {
         setInterval(() => {
             timer()
-        }, 10);    
+        }, 10);
     }
+
+    if (cont == 0) {
+        intervalo()
+    }
+
+
+
+    intervalo()
+
     return (
         <>
             <div>
                 <h2>{tempo}</h2>
+                <Button className="btn btn-dark mb-3 mt-3" onClick={() => handleClick()} id="button">{decre}</Button>
+                <Button className="btn btn-dark mb-3 mt-3" onClick={() => handleStop()} id="button">Parar</Button>
             </div>
         </>
     )
